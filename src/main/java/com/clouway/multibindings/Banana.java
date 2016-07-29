e00393d57d12ddc990a0dd8d6e74fbc924097054
Stanislava Kaukova <sisiivanovva@gmail.com>
2016-07-29 13:00:41 +0300

    guice: multibindings
    created example for multibindings

---
+++ src/main/java/com/clouway/multibindings/Banana.java
@@ -0,0 +1,10 @@
+package com.clouway.multibindings;
+
+/**
+ * @author Stanislava Kaukova(sisiivanovva@gmail.com)
+ */
+public class Banana implements Fruit {
+  public String getType() {
+    return "This type is banana\n";
+  }
+}
