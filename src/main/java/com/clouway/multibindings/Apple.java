dc1eb00d6dfe30f9b7ed292b66c10cb884c07027
Stanislava Kaukova <sisiivanovva@gmail.com>
2016-07-29 13:00:41 +0300

    guice: multibindings
    created example for multibindings

---
+++ src/main/java/com/clouway/multibindings/Apple.java
@@ -0,0 +1,11 @@
+package com.clouway.multibindings;
+
+/**
+ * @author Stanislava Kaukova(sisiivanovva@gmail.com)
+ */
+public class Apple implements Fruit {
+  @Override
+  public String getType() {
+    return "This type is apple\n";
+  }
+}
